3f0b6e0848c327f0390e26c82f3c504f87fba1f0
cosote <cosote@gmail.com>
2023-07-27 00:14:41 +0200

    Fix run.sh

--- async-tcp-proxy/run.sh
+++ async-tcp-proxy/run.sh
@@ -14,10 +14,11 @@ echo "Using implementation: $CONFIG_PROXY_IMPLEMENTATION"
 echo "Listen on port: $CONFIG_PORT"
 echo "Server: $CONFIG_SERVER_HOST:$CONFIG_SERVER_PORT"
 echo "Server timeout: $CONFIG_SERVER_TIMEOUT"
+echo "Client timeout: $CONFIG_CLIENT_TIMEOUT"
 echo "Loglevel: $CONFIG_LOGLEVEL"
 
 if [ "$CONFIG_PROXY_IMPLEMENTATION" = "default" ]; then
-  execute="python async-tcp-proxy-version-bing-bot.py --port $CONFIG_PORT --server-host $CONFIG_SERVER_HOST --server-port $CONFIG_SERVER_PORT --server-timeout $CONFIG_SERVER_TIMEOUT --client-timeout $CONFIG_CLIENT_TIMEOUT" --loglevel $CONFIG_LOGLEVEL
+  execute="python async-tcp-proxy-version-bing-bot.py --port $CONFIG_PORT --server-host $CONFIG_SERVER_HOST --server-port $CONFIG_SERVER_PORT --server-timeout $CONFIG_SERVER_TIMEOUT --client-timeout $CONFIG_CLIENT_TIMEOUT --loglevel $CONFIG_LOGLEVEL"
 else
   execute="python async-tcp-proxy.py -lp $CONFIG_PORT -ti $CONFIG_SERVER_HOST -tp $CONFIG_SERVER_PORT -tt $CONFIG_SERVER_TIMEOUT"
 fi
